9cd99de7c0c527cf93c392aeb05dce5ca58fb967
Justin Firth <jfirth@abstractsequential.com>
2011-11-15 17:31:34 -0500

    Ruby, day 1

---
+++ ruby/day1/3.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+i = 0
+while i < 10
+	puts 'Justin M. Firth'
+	i = i + 1
+end
---
+++ ruby/day1/4.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+i = 1
+while i < 11
+	puts 'This is string number ' + i.to_s()
+	i = i + 1
+end
